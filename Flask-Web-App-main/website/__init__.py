9d45be623d4ee5f9638c65185ed9d9dd1a580cee
Muhammad Shihab <md.shihabshahriarrashu@gmail.com>
2021-07-20 01:34:50 +0600

    Initial commit

---
+++ Flask-Web-App-main/website/__init__.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+
+def create_app():
+    # Initialize app
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '$rbn'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # Register external routes as blueprints
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    # Render custom Error404
+    @app.errorhandler(404)
+    def error404(error):
+        return "Custom Error Page", 404
+
+    # Initialize Database
+    from .models import User, Post
+    create_database(app)
+
+    # Initialize login manager
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+# Creates database.db file in path if doesn't exist
+def create_database(app):
+    if not path.exists('database/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database Doesn\'t exist. New database created!')
